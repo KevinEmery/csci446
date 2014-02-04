dd2743fa6a111c6bc9981630bf2c5bd9307f0450
Kevin Emery <kemery@mines.edu>
2014-02-04 13:42:32 -0700

    Completed the scaffolding, following the guide in Chapter 6

---
+++ unit4/demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '7cff1ca05d61b6fc6bf49064d0c60786cbcc3ec140b8819f9921b01b4abf343b9220e29bfd735397ce4bd96d6ebada960d32e1658d214185ae85e55f4909c9fb'
---
+++ unit4/depot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '1b23e2f6037ab3b6b8b2886fbbddc26171cfb8f75ca33031c950f59daf95cac27ee817287821bb91b6f50ab261b6c30c681490dfd5743b549cac06529e05421e'
