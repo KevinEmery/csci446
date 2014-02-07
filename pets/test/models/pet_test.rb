489606b5aeb3c7ee00925e4bb4baf0c633a0ca54
Kevin Emery <kemery@mines.edu>
2014-02-07 00:54:09 -0700

    Added unit tests to verify the back-end behavior of the validation added on the form

--- pets/test/models/pet_test.rb
+++ pets/test/models/pet_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "attributes may not be empty" do
+    pet = Pet.new
+    assert pet.invalid?
+    assert pet.errors[:name].any?
+    assert pet.errors[:breed].any?
+    assert pet.errors[:description].any?
+    assert pet.errors[:age].any?
+    assert pet.errors[:image_url].any?
+    
+  end
+  
+  test "age must be greater than or equal to zero" do
+    
+    pet = Pet.new(name: "test", 
+                  breed: "test",
+                  description: 'test',
+                  image_url: 'test.jpg')
+    
+    pet.age = -1
+    assert pet.invalid?
+    assert_equal ["must be greater than or equal to 0"], pet.errors[:age]
+    
+    pet.age = 0
+    assert pet.valid?
+    
+    pet.age = 1
+    assert pet.valid?
+  end
+  
+  # Direct port of the test used in the depot app
+  test "image url" do
+    
+    pet = Pet.new(name: "test", 
+                  breed: "test",
+                  description: 'test',
+                  age: 1)
+    
+    ok = %w{ fred.gif fred.jpg fred.png FRED.gif FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
+    bad = %w{ fred.doc fred.git/more fred.gif.more }
+    ok.each do |name|
+      pet.image_url = name
+      assert pet.valid?
+    end
+    bad.each do |name|
+      pet.image_url = name
+      assert pet.invalid?
+    end
+  end
+  
 end
